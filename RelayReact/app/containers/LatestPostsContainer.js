d2b5a981109a250a11f85836ea33ab462facc2c5
Neson <neson@dex.tw>
2016-08-14 00:06:24 +0800

    功能增強：使用者可以看到最新的 Posts (載入中提示與錯誤訊息)

--- RelayReact/app/containers/LatestPostsContainer.js
+++ RelayReact/app/containers/LatestPostsContainer.js
@@ -4,6 +4,13 @@
 
 import React, { Component } from 'react';
 import Relay from 'react-relay';
+import {
+  StyleSheet,
+  View,
+  Text,
+  TouchableOpacity,
+  ActivityIndicator
+} from 'react-native';
 
 import LatestPosts from 'components/LatestPosts';
 import SiteRoute from 'routes/SiteRoute';
@@ -14,7 +21,39 @@ export default class LatestPostsContainer extends Component {
       <Relay.RootContainer
         Component={LatestPosts}
         route={new SiteRoute()}
+        renderLoading={() => {
+          return (
+            <View style={styles.loadingMessage}>
+              <ActivityIndicator/>
+              <Text style={styles.loadingMessageText}>Loading...</Text>
+            </View>
+          );
+        }}
+        renderFailure={(error, retry) => {
+          return (
+            <View style={styles.loadingMessage}>
+              <Text style={styles.loadingMessageText}>{error.message}</Text>
+              <TouchableOpacity onPress={retry}>
+                <Text style={styles.retryText}>Retry</Text>
+              </TouchableOpacity>
+            </View>
+          );
+        }}
       />
     );
   }
 }
+
+const styles = StyleSheet.create({
+  loadingMessage: {
+    padding: 24,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  loadingMessageText: {
+    marginVertical: 16
+  },
+  retryText: {
+    textDecorationLine: 'underline'
+  }
+});

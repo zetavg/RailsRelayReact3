89375149a62dadb69edbacccaf728bc1342c7aea
Neson <neson@dex.tw>
2016-08-13 23:50:14 +0800

    功能增強：使用者可以看到最新的 Posts (無限捲軸)

--- RelayReact/app/components/SitePosts.js
+++ RelayReact/app/components/SitePosts.js
@@ -9,23 +9,46 @@ import PostsList from 'components/PostsList';
 
 class SitePosts extends Component {
   static propTypes = {
-    site: PropTypes.object.isRequired
+    site: PropTypes.object.isRequired,
+    relay: PropTypes.shape({
+      variables: PropTypes.object.isRequired,
+      setVariables: PropTypes.func.isRequired
+    }).isRequired
   }
 
   render() {
     let { site } = this.props;
 
     return (
-      <PostsList postConnection={site.posts} />
+      <PostsList
+        postConnection={site.posts}
+        onEndReached={this.loadMorePosts.bind(this)}
+      />
     );
+  }
+
+  loadMorePosts() {
+    let { pageInfo } = this.props.site.posts;
+    if (!pageInfo.hasNextPage) return;
+
+    let { postsCount } = this.props.relay.variables;
+    this.props.relay.setVariables({
+      postsCount: postsCount + 10
+    });
   }
 }
 
 export default Relay.createContainer(SitePosts, {
+  initialVariables: {
+    postsCount: 10
+  },
   fragments: {
     site: () => Relay.QL`
       fragment on Site {
-        posts(first: 10) {
+        posts(first: $postsCount) {
+          pageInfo {
+            hasNextPage
+          }
           ${PostsList.getFragment('postConnection')}
         }
       }

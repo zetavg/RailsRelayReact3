a39cf7c891c15f95e0d7dc2920a9ad62203a157e
Neson <neson@dex.tw>
2016-08-14 16:34:44 +0800

    功能：使用者可以看到 Post 的 Comments (2)
    建造 components/PostWithComments 來顯示 Post 以及其 Comments。

---
+++ RelayReact/app/components/PostWithComments.js
@@ -0,0 +1,153 @@
+/**
+ * @providesModule components/PostWithComments
+ */
+
+import React, { PropTypes, Component } from 'react';
+import Relay from 'react-relay';
+import {
+  StyleSheet,
+  ListView,
+  View,
+  RefreshControl
+} from 'react-native';
+
+import Post from 'components/Post';
+import Comment from 'components/Comment';
+
+const dataSource = new ListView.DataSource({
+  rowHasChanged: (r1, r2) => r1.__dataID__ !== r2.__dataID__
+});
+
+const getDataSourceFromProps = (props) => {
+  return dataSource.cloneWithRows([
+    props.post,
+    null,
+    ...props.post.comments.edges
+  ]);
+};
+
+class PostWithComments extends Component {
+  static propTypes = {
+    post: PropTypes.object.isRequired,
+    refreshing: PropTypes.bool,
+    onRefresh: PropTypes.func
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      dataSource: getDataSourceFromProps(props)
+    };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props.post !== nextProps.post ||
+        this.props.post.comments.edges !== nextProps.post.comments.edges) {
+      this.setState({
+        dataSource: getDataSourceFromProps(nextProps)
+      });
+    }
+  }
+
+  renderRow(rowData, sectionID, rowID) {
+    if (rowID === '0') { // post
+      return <Post post={rowData} />;
+
+    } else if (rowID === '1') { // blank area
+      return <View style={styles.blankArea} />;
+
+    } else { // comment
+      return <Comment comment={rowData.node} />;
+    }
+  }
+
+  renderSeparator(sectionID, rowID) {
+    if (rowID === '0') {
+      return (
+        <View
+          key={`sep-${sectionID}-${rowID}`}
+        />
+      );
+    } else if (rowID === '1') {
+      return (
+        <View
+          key={`sep-${sectionID}-${rowID}`}
+          style={styles.heavySeparator}
+        />
+      );
+
+    } else if (sectionID === 'footer') {
+      return (
+        <View
+          key={`sep-${sectionID}-${rowID}`}
+          style={[styles.heavySeparator, styles.blankArea]}
+        />
+      );
+
+    } else {
+      return (
+        <View
+          key={`sep-${sectionID}-${rowID}`}
+          style={styles.separator}
+        />
+      );
+    }
+  }
+
+  render() {
+    return (
+      <ListView
+        refreshControl={
+          this.props.onRefresh ?
+          <RefreshControl
+            refreshing={this.props.refreshing}
+            onRefresh={this.props.onRefresh}
+          />
+          :
+          null
+        }
+        dataSource={this.state.dataSource}
+        renderRow={this.renderRow.bind(this)}
+        renderSeparator={this.renderSeparator.bind(this)}
+        renderFooter={this.renderSeparator.bind(this, 'footer')}
+      />
+    );
+  }
+}
+
+export default Relay.createContainer(PostWithComments, {
+  initialVariables: {
+    commentsCount: 5
+  },
+  fragments: {
+    post: () => Relay.QL`
+      fragment on Post {
+        ${Post.getFragment('post')},
+        comments(first: 10) {
+          edges {
+            node {
+              ${Comment.getFragment('comment')}
+            }
+          }
+        }
+      }
+    `
+  }
+});
+
+const styles = StyleSheet.create({
+  blankArea: {
+    marginBottom: 16
+  },
+  separator: {
+    borderLeftWidth: 15,
+    borderRightWidth: 15,
+    borderColor: '#FFF',
+    height: StyleSheet.hairlineWidth,
+    backgroundColor: '#E4EBF1'
+  },
+  heavySeparator: {
+    height: StyleSheet.hairlineWidth,
+    backgroundColor: '#313B4716'
+  }
+});
